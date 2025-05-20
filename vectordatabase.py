36a35cb6e732d1157627cd348cb9acebbedf4d57
Dedeep Vasireddy <vasireddydedeep@gmail.com>
2025-05-20 22:33:14 +0530

    html parser => lxml parser in vec db

--- vectordatabase.py
+++ vectordatabase.py
@@ -22,7 +22,7 @@ class VectorDatabase:
 
     def scrape_web(self, url):
         response = requests.get(url)
-        soup = BeautifulSoup(response.text, "html.parser")
+        soup = BeautifulSoup(response.text, "lxml")
         text = soup.get_text(separator=" ", strip=True)
         return text
 
