12a19b507fbc8fe40389d34675c503c4de0755b6
aiclubiitropar <116072638+aiclubiitropar@users.noreply.github.com>
2025-06-10 12:46:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -265,5 +265,6 @@ def cleanup():
     except Exception:
         pass
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True, threaded=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
